182a945a14226ea146bb589baf083318e6de6a53
davincidreams <lisamegawatts@gmail.com>
2023-07-18 07:42:56 -0400

    logo change

--- frontend/lib/components/NavBar/components/Logo.tsx
+++ frontend/lib/components/NavBar/components/Logo.tsx
@@ -7,11 +7,11 @@ export const Logo = (): JSX.Element => {
       <Image
         className="rounded-full"
         src={"/logo.png"}
-        alt="Quivr Logo"
+        alt="Max Smart"
         width={48}
         height={48}
       />
-      <h1 className="font-bold">Quivr</h1>
+      <h1 className="font-bold">Max Smart</h1>
     </Link>
   );
 };
